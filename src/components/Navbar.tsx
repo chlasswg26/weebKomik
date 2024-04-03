9938cad713ba50d24fba217e9a6df8b5ef380529
Ichlas Wardy Gustama <cahyo4713@gmail.com>
2024-04-03 10:23:12 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <>
       <nav style={{ borderBottom: "1px solid lightgrey", marginBottom: "1em" }}>
         <ul>
-          <li><a href={'/'}>&emsp; <img width={'50px'} src={Logo} alt="logo" /> <b> W</b>eeb<b>K</b>omik </a></li>
+          <li><a href={'/'}>&emsp; <img width={'50px'} src={Logo} alt="logo" /> <b> N</b>ia<b> W</b>arda<b> K</b>omik </a></li>
         </ul>
         <ul>
           <li><Link to={'/'}><iframe src="https://ghbtns.com/github-btn.html?user=fajarspace&repo=weebKomik&type=star&size=large&text=false" width="60" height="30" title="GitHub"></iframe></Link></li>
